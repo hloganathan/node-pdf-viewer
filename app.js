a290f58b96b54ef19e25a7b0435663ee0e0c7030
Matt Lo <mattlo.developer@gmail.com>
2015-02-10 11:34:57 -0600

    fixed indentation

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 var http = require('http'),
-	fs = require('fs');
+  fs = require('fs');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {
-  	'Content-Type': 'application/pdf',
-  	'Content-Disposition': 'inline; filename="peters-mom.pdf"'
+    'Content-Type': 'application/pdf',
+    'Content-Disposition': 'inline; filename="peters-mom.pdf"'
   });
 
   fs.readFile('./subject.pdf', function (err, data) {
-  	if (err) {
-  		res.end(err.toString());
-  	}
+    if (err) {
+      res.end(err.toString());
+    }
 
-  	res.end(data);
+    res.end(data);
   });
 }).listen(9111, '127.0.0.1');
 
